571da88588dc04bbf6e8e07e1cd959096ebca24a
Douglas Almeida <doug19almeida@gmail.com>
2023-06-03 16:30:59 -0300

    Ajustes

---
+++ tcc-spring/src/test/java/com/claridoug/tccspring/TccSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claridoug.tccspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
