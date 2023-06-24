3c12b03321a433ae8c9bb164a0a9a34e973ee950
ClarisseDinizS <dinizclarisse.cd@gmail.com>
2023-06-24 13:34:22 -0300

    Criação do Serviço Usuário

---
+++ tcc-spring/src/main/java/Exceptions/RegraNegocioException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+public class RegraNegocioException extends RuntimeException {
+
+    public RegraNegocioException(String message){
+        super(message);
+    }
+
+}
--- tcc-spring/src/main/java/com/claridoug/tccspring/Service/Controller/UsuarioController.java
+++ tcc-spring/src/main/java/com/claridoug/tccspring/Service/Controller/UsuarioController.java
@@ -1,13 +1,18 @@
 package com.claridoug.tccspring.Service.Controller;
 
+import Exceptions.RegraNegocioException;
 import com.claridoug.tccspring.Service.UsuarioService;
 import com.claridoug.tccspring.model.entity.Usuario;
 import com.claridoug.tccspring.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UsuarioController implements UsuarioService {
 
     private UsuarioRepository repository;
 
+    @Autowired
     public UsuarioController(UsuarioRepository repository) {
         super();
         this.repository = repository;
@@ -25,6 +30,10 @@ public class UsuarioController implements UsuarioService {
 
     @Override
     public void validarEmail(String email) {
+        boolean existe = repository.existsByEmail(email);
+        if(existe) {
+            throw new RegraNegocioException("Já existe um usuário com esse email!");
+        }
 
     }
 }
